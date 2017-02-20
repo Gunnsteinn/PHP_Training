1b4617e8f128a2d1d22ad186264b6405b513d776
gunnsteinn <guillecolo@gmail.com>
2017-02-20 13:38:58 -0300

    commit

---
+++ 20_ejemplo_variables-02.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$bool = TRUE;   // un valor booleano
+$str  = "foo";  // una cadena
+$int  = 12;     // un entero
+
+echo gettype($bool); // imprime "boolean"
+echo "<br/>";
+echo gettype($str);  // imprime "string"
+echo "<br/>";
+echo gettype($int);  // imprime "int"
+?> 

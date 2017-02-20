f58c0a34f8b86fc219ecce0ef6508502a7f39045
gunnsteinn <guillecolo@gmail.com>
2017-02-20 14:25:35 -0300

    commit

--- 19_ejemplo_variables-01.php
+++ 19_ejemplo_variables-01.php
@@ -1,13 +1,16 @@
 <?php
+/* Genere el código PHP que defina una variable del tipo entero, una doble, una cadena de caracteres y una booleana. Luego mostrarlas en pantalla, una por línea. */
 
+	$bool = TRUE;   // un valor booleano
+	$str  = "Hello World";  // una cadena
+	$int  = 2147483647;     // un entero
+	$double  = 2147483648;     // un float
 
-$bool = TRUE;   // un valor booleano
-$str  = "foo";  // una cadena
-$int  = 12;     // un entero
-
-echo gettype($bool); // imprime "boolean"
-echo "<br/>";
-echo gettype($str);  // imprime "string"
-echo "<br/>";
-echo gettype($int);  // imprime "int"
+	echo  "booleana   => ".$bool."<br/>"; // imprime "boolean"
+	echo "<br/>";
+	echo  "caracteres => ".$str."<br/>";  // imprime "string"
+	echo "<br/>";
+	echo  "entero     => ".$int."<br/>";  // imprime "int"
+	echo "<br/>";
+	echo  "doble      => ".$double."<br/>";  // imprime "int"
 ?> 
--- 20_ejemplo_variables-02.php
+++ 20_ejemplo_variables-02.php
@@ -1,13 +1,9 @@
 <?php
+/*	Genere el código PHP que defina tres variables enteras. Luego definir una cadena de caracteres que incorpore dichas variables y las sustituya en tiempo de ejecución. Recordar que una variable se sustituye cuando el string está encerrado por comillas dobles. */
 
+	$int1 = 100;   // int
+	$int2 = 1000;  // int
+	$int3 = 10000; // int
 
-$bool = TRUE;   // un valor booleano
-$str  = "foo";  // una cadena
-$int  = 12;     // un entero
-
-echo gettype($bool); // imprime "boolean"
-echo "<br/>";
-echo gettype($str);  // imprime "string"
-echo "<br/>";
-echo gettype($int);  // imprime "int"
-?> 
+	echo  " => $int1 <= <br/> => $int2 <= <br/> => $int3 <= <br/>"; // imprime las variables enteras como caracteres. 
+?>
